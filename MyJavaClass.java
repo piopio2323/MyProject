b3b94df8ea733f222edba965649d7bf0f91fb857
David Houston <djhouston2020@outlook.com>
2025-01-29 20:35:57 +0000

    Added Java Class with multiplication method

---
+++ MyJavaClass.java
@@ -0,0 +1,10 @@
+public class MyJavaClass {
+    public static int multiplyTwoNumbers(int x, int y) {
+        return x * y;
+    }
+
+    public static void main(String[] args) {
+        int result = multiplyTwoNumbers(5, 10);
+        System.out.println("Product: " + result);
+    }
+}
